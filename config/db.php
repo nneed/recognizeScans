cfbf05be2bf0441fb82f582f17b8691ab126c79f
Kazankov <nukazankov@rus-telecom.ru>
2017-12-11 10:33:53 +0300

    КОнфиг дб для разных баз

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-
+//Закоментировать на боевом
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'sqlite:/usr/local/www/apache24/data/test-site/yii2/queue.db',
@@ -10,3 +10,16 @@ return [
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
 ];
+//Раскоментировать на боевом
+/*return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=queue',
+    'charset' => 'utf8',
+    'username' => 'postgres',
+    'password' => '',
+
+    // Schema cache options (for production environment)
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
+];*/
