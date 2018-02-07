72efe1b8d2a483ea31a19eb0bba88e40f6167ade
Kazankov <nukazankov@rus-telecom.ru>
2018-02-07 12:33:57 +0300

    Переход на postgresql

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 //Закоментировать на боевом
-return [
+/*return [
     'class' => 'yii\db\Connection',
     'dsn' => 'sqlite:/usr/local/www/apache24/data/test-site/yii2/queue.db',
     'charset' => 'utf8',
@@ -9,9 +9,9 @@ return [
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
-];
+];*/
 //Раскоментировать на боевом
-/*return [
+return [
     'class' => 'yii\db\Connection',
     'dsn' => 'pgsql:host=localhost;port=5432;dbname=queue',
     'charset' => 'utf8',
@@ -22,4 +22,4 @@ return [
     'enableSchemaCache' => true,
     'schemaCacheDuration' => 60,
     'schemaCache' => 'cache',
-];*/
+];
